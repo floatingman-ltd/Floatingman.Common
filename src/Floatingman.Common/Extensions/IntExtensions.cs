1cff41b8f944656a9cfd9225e08e3fa1ee865600
waltiam <2963975+waltiam@users.noreply.github.com>
2022-01-15 22:50:20 +0000

    ✨ int extensions

---
+++ src/Floatingman.Common/Extensions/IntExtensions.cs
@@ -0,0 +1,11 @@
+using Floatingman.Common.Functional;
+using static Floatingman.Common.Functional.Functional;
+
+namespace Floatingman.Common.Extensions
+{
+    public static class IntExtensions
+    {
+        public static Option<int> Parse(string s)
+            => int.TryParse(s, out var r) ? Some(r) : None;
+    }
+}
